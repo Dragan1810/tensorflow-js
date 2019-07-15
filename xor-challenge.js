55f645bc22a2ec9f20efc6edad64496d60dcc822
Dragan <dragan.novakovic@bridgewaterlabs.com>
2019-07-15 08:47:42 +0200

    linear stuff

--- xor-challenge.js
+++
@@ -1,8 +0,0 @@
-const trainingData = [];
-
-const model = tf.sequential({
-  layers: [
-    tf.layers.dense({ inputShape: [784], units: 32, activation: 'relu' }),
-    tf.layers.dense({ units: 10, activation: 'softmax' })
-  ]
-});
