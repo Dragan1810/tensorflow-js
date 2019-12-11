1c3969b8691f263cb64104c4e87fb854ca71ea60
dragan1810 <d.novakovic1810@yahoo.com>
2019-12-11 19:33:00 +0100

    qqq

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Webcam from "react-webcam";
-import gant from "./corn.png";
 import "./App.css";
 import { RPSDataset } from "./tfjs/data.js";
 import { getAdvancedModel, getSimpleModel } from "./tfjs/models.js";
@@ -31,7 +30,6 @@ class App extends Component {
       return (
         <div>
           <AdvancedModel key="advancedDemo" />
-          <p>Turn off ad-block where applicable</p>
         </div>
       );
     }
@@ -89,12 +87,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="Main">
-          <p>
-            We'll show progress in the TensorFlow.js Vis panel. You'll see it
-            when you click the load and show button below. Press{" "}
-            <span className="cod">`</span> or <span className="cod">~</span> key
-            to hide this menu.
-          </p>
           <button
             className="myButton"
             onClick={async () => {
@@ -108,32 +100,6 @@ class App extends Component {
           >
             {this.state.loadDataMessage}
           </button>
-          <p>
-            Each of the examples have been loaded now. Due to this being a
-            browser, the data is loaded with one{" "}
-            <a href="./data.png" target="_blank" rel="noopener noreferrer">
-              sprite-sheet
-            </a>{" "}
-            to get around sandboxing. My code to create sprite-sheets is
-            available with{" "}
-            <a
-              href="https://github.com/GantMan/rps_tfjs_demo"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              this repo on GitHub
-            </a>
-            .
-          </p>
-          <p>
-            You now create the structure for the data, that hopefully works
-            best.{" "}
-            <strong>
-              In this situation, an advanced model is a bad choice.
-            </strong>{" "}
-            An advanced model will train slower while overfitting this small and
-            simple training data.
-          </p>
           <div className="GroupUp">
             <button
               className={
@@ -188,10 +154,6 @@ class App extends Component {
           >
             Check Untrained Model Results
           </button>
-          <p>
-            Train your Model with your training data. In this case 2100 labeled
-            images, over and over... but not <em>toooooo much.</em>
-          </p>
           <button
             className="myButton"
             onClick={async () => {
@@ -205,11 +167,6 @@ class App extends Component {
             Train Your {this.state.currentModel} Model
           </button>
           <p>
-            Now that our model has seen some stuff{" "}
-            <span role="img" aria-label="woah">
-              😳
-            </span>
-            <hr />
             It should be smarter at identifying RPS! We can now test it with 420
             RPS images it's never seen before.
           </p>
@@ -229,39 +186,6 @@ class App extends Component {
           >
             Check Model After Training
           </button>
-          <p>
-            We can now save our trained model! We can store it via downloading
-            it, uploading it, or place the results in localstorage for access of
-            the browser.
-          </p>
-          <p>
-            The simple model size comes out to about 48Kb, but some models can
-            be as large as 20+MBs! It depends how simple you keep the model. If
-            you want the model trained above, you get two files by{" "}
-            <a
-              className="pointy"
-              onClick={async () => {
-                if (!this.model) return;
-                await this.model.save("downloads://rps-model");
-              }}
-            >
-              clicking here
-            </a>
-            . The <span className="cod">model.json</span> file demonstrates the
-            structure of the model, and the weights are our non-random trained
-            values that make the model accurate.
-          </p>
-          <h3>Now let's see if we can test our model with the real world!</h3>
-          <img src="./rps_webcam_big.jpg" className="demo" alt="webcam demo" />
-          <p>
-            Keep in mind, the training data for this model had no background,
-            and the model itself isn't practiced in dealing with noise and
-            rotation. A more advanced model would do better, but for this demo
-            you shouldn't have any problems getting consistent and accurate
-            results. When testing on a webcam, you'll need to make the images as
-            clean as you can. Every few seconds your webcam image will be
-            converted to a 64x64 grayscale image for your model to classify.
-          </p>
           <button
             className="myButton"
             onClick={async () => {
@@ -280,11 +204,6 @@ class App extends Component {
             {this.state.webcamActive ? "Turn Webcam Off" : "Launch Webcam"}
           </button>
           {this._renderWebcam()}
-          <p>
-            Did our model work for you? Maybe it did, and maybe it didn't! It's
-            a very simple model that we've created on very simple data. Don't
-            feel bad if it didn't work.
-          </p>
           <p>
             What does it look like to train a far more advanced model for hours
             that results in a 20+MB model? Here's an opportunity for you to try
@@ -305,143 +224,6 @@ class App extends Component {
               : "Show Advanced Demo"}
           </button>
           {this._renderAdvancedModel()}
-          <p>
-            Machine Learning is exciting! And now you're part of it, as you
-            trained a model right in your browser. We've only scratched the
-            surface of what you can build. Automating with ML on computers is
-            only limited by our imagination!
-          </p>
-          <p>
-            If you'd like to see more applications of TensorFlow.js be sure to
-            check out{" "}
-            <a
-              href="https://nsfwjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              NSFWJS.com
-            </a>
-            , or the very useful{" "}
-            <a
-              href="https://nicornot.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              NicOrNot.com
-            </a>
-            . For more entertaining applications of Machine Learning, be sure to
-            subscribe to our{" "}
-            <a
-              href="https://infinite.red/machinelearning"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Newsletter
-            </a>{" "}
-            or follow my Fun Machine Learning Twitter account:
-          </p>
-
-          <a
-            href="https://twitter.com/FunMachineLearn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src="fml.png" style={{ width: "50%", marginLeft: "25%" }} />
-            <p style={{ textAlign: "center" }}>@FunMachineLearn</p>
-          </a>
-        </div>
-        <div className="GroupUp">
-          <p className="outro">
-            Follow me (Gant Laborde) and Infinite Red for cool new experiments,
-            and let us know what cool things you've come up with.{" "}
-            <em>
-              We can help, we're available for AI consulting and training.
-            </em>
-          </p>
-        </div>
-        <div className="GroupUp">
-          <img src={gant} className="wiggle me" alt="Gant Laborde" />
-          <ul id="footer">
-            <li>
-              Website:{" "}
-              <a
-                href="http://gantlaborde.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GantLaborde.com
-              </a>
-            </li>
-            <li>
-              Twitter:{" "}
-              <a
-                href="https://twitter.com/gantlaborde"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                @GantLaborde
-              </a>
-            </li>
-            <li>
-              Medium:{" "}
-              <a
-                href="https://medium.freecodecamp.org/@gantlaborde"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GantLaborde
-              </a>
-            </li>
-            <li>
-              ML Twitter:{" "}
-              <a
-                href="https://twitter.com/FunMachineLearn"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                FunMachineLearn
-              </a>
-            </li>
-            <li>
-              GitHub:{" "}
-              <a
-                href="https://github.com/GantMan/rps_tfjs_demo"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                RPS TFJS Demo
-              </a>
-            </li>
-            <li>
-              Newsletter:{" "}
-              <a
-                href="https://infinite.red/machinelearning"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                ML Newsletter
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://infinite.red"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src="./ir.svg" id="InfiniteRed" alt="Infinite Red" />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="GroupUp">
-          <img src="./ml.png" id="closer" />
-          <h4>powered by</h4>
-          <img
-            src="./TF_FullColor_Horizontal.png"
-            id="closer"
-            alt="Tensorflow logo"
-            style={{ paddingLeft: "-40px" }}
-          />
         </div>
       </div>
     );
